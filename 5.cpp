983a95ea8712fe3d9c9d12ad1f3f13a01ef7a96c
Trisha Moitra <trishamoitra.217@gmail.com>
2021-09-12 22:36:24 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -14,7 +14,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-void pattern(int n)
+void pattern1(int n)                     //using 2 nested for loops
 {
     for(int row=1;row<=n;row++)
     {
@@ -34,10 +34,33 @@ void pattern(int n)
     }
 }
 
+void pattern2(int n)                            //using 1 nested for loop
+{
+    int totalColsInEachRow;
+    for(int row=0;row<2*n;row++)
+    {
+        //for every row, run the col
+        if(row<n)
+        {        
+            totalColsInEachRow = row;
+        }
+        else{
+            totalColsInEachRow = (2*n) - row;
+        }
+        for(int col=0;col<totalColsInEachRow;col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }   
+}
+
+
 int main()
 {
     int n;
     cin>>n;
-    pattern(n);
+    pattern1(n);
+    pattern2(n);
     return 0;
 }
