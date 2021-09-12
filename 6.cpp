96d56aaabe5b6f74f77bca8254760a0987407dcf
Trisha Moitra <trishamoitra.217@gmail.com>
2021-09-12 23:14:13 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,39 @@
+/*
+
+
+6.       *
+        **
+       ***
+      ****
+     *****
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+{
+    int noOfSpaces;
+    for(int row=1;row<=n;row++)
+    {
+        noOfSpaces = n-row;
+        for(int col = 1;col<=n;col++){
+           if(col>noOfSpaces)
+           {
+               cout<<"*";
+           }
+           else{
+               cout<<" ";
+           }
+        }
+        cout<<endl;
+    }   
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
