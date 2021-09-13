3fdffc1af5d7f65a3c8b7aef1b288a2b7c7fe8d7
Trisha Moitra <trishamoitra.217@gmail.com>
2021-09-13 12:55:45 +0530

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,76 @@
+/*
+
+
+
+18.   **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+{
+
+    for (int row = 0; row < (2 * n); row++)
+    {
+        if (row < n)
+        {
+            int noOfStarsBeforeN = n - row;
+            int noOfStarsAfterN = n + row;
+            int flag = 0;
+            for (int col = 1; col < (2 * n); col++)
+            {
+                if (col <= noOfStarsBeforeN || col >= noOfStarsAfterN)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+        else if (row > n)
+        {
+            int row1 = row-n;
+            int noOfSpacesBeforeN = row1 + 1;
+            int noOfSpacesAfterN = (2 * n) - row1 - 1;
+            int flag = 0;
+            for (int col = 1; col < (2 * n); col++)
+            {
+                if (col <= noOfSpacesBeforeN || col >= noOfSpacesAfterN)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+        else
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
